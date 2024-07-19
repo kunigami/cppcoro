175992b806985bc41fea714a5eb0ddb9338dc64a
Guilherme Kunigami <kunigami@fb.com>
2024-07-19 08:55:19 -0700

    de-templatize when_all_readhy.hpp

--- include/cppcoro/when_all_ready.hpp
+++ include/cppcoro/when_all_ready.hpp
@@ -2,12 +2,12 @@
 // Copyright (c) Lewis Baker
 // Licenced under MIT license. See LICENSE.txt for details.
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef CPPCORO_WHEN_ALL_READY_HPP_INCLUDED
-#define CPPCORO_WHEN_ALL_READY_HPP_INCLUDED
+#pragma once
 
 #include <cppcoro/config.hpp>
 #include <cppcoro/awaitable_traits.hpp>
 #include <cppcoro/is_awaitable.hpp>
+#include <cppcoro/task.hpp>
 
 #include <cppcoro/detail/when_all_ready_awaitable.hpp>
 #include <cppcoro/detail/when_all_task.hpp>
@@ -18,39 +18,17 @@
 #include <vector>
 #include <type_traits>
 
-namespace cppcoro
-{
-	template<
-		typename... AWAITABLES,
-		std::enable_if_t<std::conjunction_v<
-			is_awaitable<detail::unwrap_reference_t<std::remove_reference_t<AWAITABLES>>>...>, int> = 0>
-	[[nodiscard]]
-	CPPCORO_FORCE_INLINE auto when_all_ready(AWAITABLES&&... awaitables)
-	{
-		return detail::when_all_ready_awaitable<std::tuple<detail::when_all_task<
-			typename awaitable_traits<detail::unwrap_reference_t<std::remove_reference_t<AWAITABLES>>>::await_result_t>...>>(
-				std::make_tuple(detail::make_when_all_task(std::forward<AWAITABLES>(awaitables))...));
+namespace cppcoro {
+
+auto when_all_ready(std::vector<Task> awaitables) {
+	std::vector<detail::when_all_task<std::string&>> tasks;
+
+	tasks.reserve(awaitables.size());
+	for (auto& awaitable : awaitables) {
+		tasks.emplace_back(detail::make_when_all_task(std::move(awaitable)));
 	}
 
-	// TODO: Generalise this from vector<AWAITABLE> to arbitrary sequence of awaitable.
-
-	template<
-		typename AWAITABLE,
-		typename RESULT = typename awaitable_traits<detail::unwrap_reference_t<AWAITABLE>>::await_result_t>
-	[[nodiscard]] auto when_all_ready(std::vector<AWAITABLE> awaitables)
-	{
-		std::vector<detail::when_all_task<RESULT>> tasks;
-
-		tasks.reserve(awaitables.size());
-
-		for (auto& awaitable : awaitables)
-		{
-			tasks.emplace_back(detail::make_when_all_task(std::move(awaitable)));
-		}
-
-		return detail::when_all_ready_awaitable<std::vector<detail::when_all_task<RESULT>>>(
-			std::move(tasks));
-	}
+	return detail::when_all_ready_awaitable<std::vector<detail::when_all_task<std::string&>>>(
+		std::move(tasks));
 }
-
-#endif
+} // namespace cppcoro
